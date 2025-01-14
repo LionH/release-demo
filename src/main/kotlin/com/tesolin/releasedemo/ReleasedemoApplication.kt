b1af62c2299eaa5bd79c643c6216ab5828bfd7b0
Lionel Tesolin <liontes@gmail.com>
2025-01-14 11:14:39 +0100

    Initial project setup

---
+++ src/main/kotlin/com/tesolin/releasedemo/ReleasedemoApplication.kt
@@ -0,0 +1,11 @@
+package com.tesolin.releasedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReleasedemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReleasedemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/tesolin/releasedemo/ReleasedemoApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.tesolin.releasedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReleasedemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
